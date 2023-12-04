7ad36d33b17cea433a241be53dd1989bbe78df22
Tray Berry <trayberry@comcast.net>
2023-12-04 12:01:33 -0500

    Continued working on dec-3 p1. Solution is almost working; need to fix the minesweep function so it appropriately identifies mines.

--- dec-3/p1/solution.py
+++ dec-3/p1/solution.py
@@ -1,15 +1,17 @@
 engineFile = open('engineSchematic.txt','r')
 
 engineArray = []
-count = 0
 
 # Build engineFile with each character having its own index in the array
+count = 0
 for row in engineFile:
     engineArray.append(row.strip())
     engineArray[count] = list(engineArray[count])
     count += 1
 
-# Seek symbols which are not numeric in any given line of the engineFile
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Function which seeks symbols that aren't numeric in any given array of characters and returns an array of their indices.
 def seekMine(engineArray):
     count = 0
     arrayOfIndices = []
@@ -19,6 +21,64 @@ def seekMine(engineArray):
         count += 1
     return arrayOfIndices
 
-# Seek mines in each row and return an array of indices
+# Extract a number from an engineArray line based on a single coordinate.
+def locateNumber(row, x, y):
+    numBuilder = ''
+    for q in range(y-2, y+2):
+        if row[x][q].isnumeric():
+            numBuilder += str(row[x][q])
+    return numBuilder
+
+# Sweep the engine array's specific coordinate for digestable numbers. Add the numbers to an array, convert those numbers to dots
+# in our schematic copy, and then return both the valid number and the new engine array.
+def digestNumber(engineArr, x, y):
+    numeric = True
+    currY = y
+    validInt = ''
+    while numeric:
+        if engineArr[x][currY].isnumeric():
+            validInt+=str(engineArr[x][currY])
+            engineArr[x][currY] = '.'
+        else:
+            numeric = False
+        if(currY>=len(engineArr[x])-1):
+            break
+        else:
+            currY+=1
+
+    return [validInt, engineArr]
+
+
+
+# Sweep the original engine array on a particular coordinate and return all numbers which surround that coordinate.
+# PROBLEM: Currently, the following would be considered a mine: 5..*... -- The code captures the 5.
+def minesweep(engineArr, coord):
+    nearbyMines = []
+    for x in range(coord[0]-1, coord[0]+1):
+        for y in range(coord[1]-3, coord[1]+3): # Only search coord[1]-1,coord[1]+1; start searching outward from there.
+            if(engineArr[x][y].isnumeric()):
+                results = digestNumber(engineArr, x, y)
+                engineArr = results[1]
+                nearbyMines.append(results[0])
+    return nearbyMines
+            
+
+# Construct an array of tuples. Each tuple is a set of coordinates which points to a 'mine'
+mineIndices = []
+count = 0
 for row in engineArray:
-    print(seekMine(row))
+    mineIndices += map(lambda x:[count, x], seekMine(row))
+    count+=1
+
+# Minesweep all mine coordinates. Creates a matrix; each index contains a list of valid numbers which were on that mine.
+mines = []
+for index in mineIndices:
+    mines.append(minesweep(engineArray,index))
+print(mines)
+
+# Add all of the valid numbers together to get our puzzle result.
+grandTotal = 0
+for mineList in mines:
+    for mines in mineList:
+        print(mines)
+print('Grand total: ',grandTotal)
