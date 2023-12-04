e1ef13a7e713bf50e92c113d7b91259d7a4aee5f
Tray Berry <trayberry@comcast.net>
2023-12-04 12:57:29 -0500

    December 3, part 1 completed

--- dec-3/p1/solution.py
+++ dec-3/p1/solution.py
@@ -24,7 +24,7 @@ def seekMine(engineArray):
 # Extract a number from an engineArray line based on a single coordinate.
 def locateNumber(row, x, y):
     numBuilder = ''
-    for q in range(y-2, y+2):
+    for q in range(y-2, y+3):
         if row[x][q].isnumeric():
             numBuilder += str(row[x][q])
     return numBuilder
@@ -35,6 +35,15 @@ def digestNumber(engineArr, x, y):
     numeric = True
     currY = y
     validInt = ''
+    #find start of number
+    while True:
+        if currY == 0:
+            break
+        if engineArr[x][currY-1].isnumeric():
+            currY -= 1
+        else:
+            break
+    #from start of number, read numeric characters
     while numeric:
         if engineArr[x][currY].isnumeric():
             validInt+=str(engineArr[x][currY])
@@ -54,13 +63,15 @@ def digestNumber(engineArr, x, y):
 # PROBLEM: Currently, the following would be considered a mine: 5..*... -- The code captures the 5.
 def minesweep(engineArr, coord):
     nearbyMines = []
-    for x in range(coord[0]-1, coord[0]+1):
-        for y in range(coord[1]-3, coord[1]+3): # Only search coord[1]-1,coord[1]+1; start searching outward from there.
+    for x in range(coord[0]-1, coord[0]+2):
+        for y in range(coord[1]-1, coord[1]+2): # Only search coord[1]-1,coord[1]+1; start searching outward from there.
+            #if x < 5:
+            #    print(x,' ',y,' ',engineArr[x][y])
             if(engineArr[x][y].isnumeric()):
                 results = digestNumber(engineArr, x, y)
                 engineArr = results[1]
                 nearbyMines.append(results[0])
-    return nearbyMines
+    return [nearbyMines, engineArr]
             
 
 # Construct an array of tuples. Each tuple is a set of coordinates which points to a 'mine'
@@ -69,16 +80,21 @@ count = 0
 for row in engineArray:
     mineIndices += map(lambda x:[count, x], seekMine(row))
     count+=1
+print('Number of mine indices: ',len(mineIndices))
 
 # Minesweep all mine coordinates. Creates a matrix; each index contains a list of valid numbers which were on that mine.
 mines = []
 for index in mineIndices:
-    mines.append(minesweep(engineArray,index))
+    results = minesweep(engineArray, index)
+    engineArray = results[1]
+    #results[0].append(index) # Utilize this line to add the index to each set of mines allowing for spot-checking of the engine file
+    mines.append(results[0])
 print(mines)
+print('Len of mines array',len(mines))
 
 # Add all of the valid numbers together to get our puzzle result.
 grandTotal = 0
 for mineList in mines:
     for mines in mineList:
-        print(mines)
+        grandTotal = grandTotal + int(mines)
 print('Grand total: ',grandTotal)
