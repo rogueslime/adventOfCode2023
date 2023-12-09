4e50f7c5cc21e4e2220739d7b6b733a713ef306d
Tray Berry <trayberry@comcast.net>
2023-12-09 13:56:16 -0500

    Dec 5 p1 kept working, moved away from dictionary building due to size of dataset

--- dec-5/p1/solution.py
+++ dec-5/p1/solution.py
@@ -1,12 +1,47 @@
 # Start with our initial seeds. Ingest those seeds and store them in an array for processing.
 
-# Create a function to decode maps. Each time an empty newline is detected run the map decoding function
-# on the next set of data. Create a function to apply this map to the seed array. Iteratively do this until
-# we have reached the end of the file, then print out our array of converted seeds.
+# destination range, source range, range length
+
+# Creating a decoded map with key:value pairs would take too long and be too large of a data structure. Instead,
+# complete the problem using arithmetic. For a seed, see if it's larger than the minimum source range length and smaller than
+# maximum source range length. If it is, calculate the destination seed by adding the difference of the source minimum and the
+# initial seed value. and pass to the next map.
+
+# Takes a seed and converts it by the arithmetic outlined in the line. Returns the converted seed.
+def conversionMath(seed, line):
+    arithmeticValues = line.split(' ')
+    if (seed == 'seeds:'):
+        return seed
+    seed = int(seed)
+    #print('lower limit: ',int(arithmeticValues[1]))
+    #print('upper limit: ',(int(arithmeticValues[1]) + int(arithmeticValues[2])))
+    if (seed >= int(arithmeticValues[1]) and seed <= (int(arithmeticValues[1]) + int(arithmeticValues[2]))):
+        print('this seed stinks: ',seed)
+        seed = int(arithmeticValues[0]) + (seed - int(arithmeticValues[1]))
+    return seed
 
 with open('mapFile.txt','r') as mapFile:
-    targetSeeds = mapFile.readline()    # capture first line as target seeds and move forward
+    targetSeeds = mapFile.readline().split(' ')    # capture first line as target seeds and move forward
+    print('targets: ',targetSeeds)
     for line in mapFile:
-        if line == '\n': continue       # if line is empty, pass this iteration
-        else: print('newline: ',line)   # else print out the line
-    
+        strippedLine = line.strip()
+        if strippedLine == '': continue       # if line is empty, pass this iteration
+        if (not strippedLine[0].isnumeric()): continue    # if line is non-numeric, pass this iteration
+
+        # If we make it here, we've made it to a new map
+
+        for x in range(0,len(targetSeeds)): # Pass all seeds and perform arithmetic, then modify those seeds.
+            newSeed = conversionMath(targetSeeds[x], strippedLine)
+            #print('newMap: ',newSeed)   # else print out the line
+            targetSeeds[x] = newSeed # Replace each seed with the modified seed
+        print(targetSeeds)
+
+print('Final Seeds!')
+lowestSeed = int(targetSeeds[1])
+for seed in targetSeeds:
+    if seed == 'seeds:': continue
+    seed = int(seed)
+    if seed<lowestSeed:
+        lowestSeed = int(seed)
+
+print(lowestSeed)
